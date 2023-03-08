0438093c7369cfd2dc61e82c76a4f82c5dee237b
Nejern <nejern@yandex.ru>
2023-03-09 01:06:36 +0300

    Added LSP

--- lua/plugins/packer_install.lua
+++ lua/plugins/packer_install.lua
@@ -23,4 +23,34 @@ return require("packer").startup(function()
 			require("plugins.lualine")
 		end,
 	})
+
+	-- LSP
+	use({
+		"williamboman/mason.nvim",
+		requires = {
+			"williamboman/mason-lspconfig.nvim",
+			"neovim/nvim-lspconfig"
+			--"nvim-lua/plenary.nvim",
+			--"jose-elias-alvarez/null-ls.nvim",
+			--"jay-babu/mason-null-ls.nvim",
+		},
+		config = function()
+			require("mason").setup()
+			--require("mason-null-ls").setup({
+				--automatic_setup = true,
+			--})
+			require("mason-lspconfig").setup()
+			require("mason-lspconfig").setup_handlers({
+				function(server_name)
+					require("lspconfig")[server_name].setup({})
+				end,
+				-- Next, you can provide a dedicated handler for specific servers.
+				-- For example, a handler override for the `rust_analyzer`:
+				--["rust_analyzer"] = function ()
+				--  require("rust-tools").setup {}
+				--end
+			})
+			require("plugins.lspconfig")
+		end,
+	})
 end)
