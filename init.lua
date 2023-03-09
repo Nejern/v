583c14f319bf18495402b9fd765c88132ad045d1
Nejern <nejern@yandex.ru>
2023-03-09 09:46:45 +0300

    Added base.cursor option

--- init.lua
+++ init.lua
@@ -8,6 +8,7 @@ require('plugins.packer_install')
 --[[ Options ]]--
 require('base.appearance')	-- Appearance
 require('base.colorscheme')	-- Colorscheme
+require('base.cursor')		-- Cursor
 require('base.completion')	-- Completion
 require('base.diagnostic')	-- Diagnostic
 require('base.other')		-- Other
