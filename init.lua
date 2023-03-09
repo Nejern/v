7a77a5c9a9fa87375c7c3effb663e87dde172e55
Nejern <nejern@yandex.ru>
2023-03-09 09:48:53 +0300

    Added base.panels option

--- init.lua
+++ init.lua
@@ -9,6 +9,7 @@ require('plugins.packer_install')
 require('base.appearance')	-- Appearance
 require('base.colorscheme')	-- Colorscheme
 require('base.cursor')		-- Cursor
+require('base.panels')		-- Panels
 require('base.completion')	-- Completion
 require('base.diagnostic')	-- Diagnostic
 require('base.other')		-- Other
---
+++ lua/base/panels.lua
@@ -0,0 +1,3 @@
+--[[ Panels ]]--
+opt.splitright = true	-- Split to the right
+opt.splitbelow = true 	-- Split to the bottom
