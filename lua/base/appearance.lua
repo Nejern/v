93362e680954406df478ecf2b35f48c6c826b433
Nejern <nejern@yandex.ru>
2023-03-09 13:18:58 +0300

    Code edit

--- lua/base/appearance.lua
+++ lua/base/appearance.lua
@@ -1,4 +1,5 @@
---[[ Appearance ]]--
-opt.showmode = false            -- No show mode
-opt.number = true               -- Show line numbers
-opt.signcolumn = "number"       -- Always show sign column
+--[[ Appearance ]]
+                          --
+opt.showmode = false      -- No show mode
+opt.number = true         -- Show line numbers
+opt.signcolumn = "number" -- Always show sign column
