c9c85f3e321cf89eec500236f2772601cf0e1923
Nejern <nejern@yandex.ru>
2023-03-09 12:12:55 +0300

    Added nvim-autopairs support to cmp

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -6,54 +6,50 @@ cmp.setup({
 			require('luasnip').lsp_expand(args.body) -- LuaSnip
 		end,
 	},
-
 	-- Window options
 	window = {
 		completion = cmp.config.window.bordered(),
 		documentation = cmp.config.window.bordered(),
 	},
-
 	-- Mapping
 	mapping = {
 		-- Select prev completion
-		['<C-p>'] = cmp.mapping.select_prev_item(),
-		['<S-Tab>'] = cmp.mapping.select_prev_item(),
+				['<C-p>'] = cmp.mapping.select_prev_item(),
+				['<S-Tab>'] = cmp.mapping.select_prev_item(),
 		-- Select next completion
-		['<C-n>'] = cmp.mapping.select_next_item(),
-		['<Tab>'] = cmp.mapping.select_next_item(),
+				['<C-n>'] = cmp.mapping.select_next_item(),
+				['<Tab>'] = cmp.mapping.select_next_item(),
 		-- Scroll docs
-		['<C-b>'] = cmp.mapping.scroll_docs(-4),
-		['<C-f>'] = cmp.mapping.scroll_docs(4),
+				['<C-b>'] = cmp.mapping.scroll_docs(-4),
+				['<C-f>'] = cmp.mapping.scroll_docs(4),
 		-- Complete
-		['<C-Space>'] = cmp.mapping.complete(),
+				['<C-Space>'] = cmp.mapping.complete(),
 		-- Close
-		['<C-e>'] = cmp.mapping.close(),
+				['<C-e>'] = cmp.mapping.close(),
 		-- Confirm
-		['<C-y>'] = cmp.mapping.confirm({
+				['<C-y>'] = cmp.mapping.confirm({
 			behavior = cmp.ConfirmBehavior.Insert,
 			select = true,
 		})
 	},
-
 	-- Sources
 	sources = {
 		-- file paths
 		{
 			name = 'path',
 			option = {
-				trailing_slash = false; -- Default false
-				label_trailing_slash = false; -- Default false
+				trailing_slash = false,   -- Default false
+				label_trailing_slash = false, -- Default false
 				--get_cwd = function(); -- Default returns the current working directory of the current buffer
 			}
 		},
-		{ name = 'nvim_lsp', keyword_length = 3 },	-- from language server
-		{ name = 'nvim_lsp_signature_help'},		-- display function signatures with current parameter emphasized
-		{ name = 'nvim_lua', keyword_length = 2},	-- complete neovim's Lua runtime API such vim.lsp.*
-		{ name = 'buffer', keyword_length = 2 },	-- source current buffer
-		{ name = 'calc'},				-- source for math calculation
-		{ name = 'luasnip', keyword_length = 2 },	-- nvim-cmp source for LuaSnip
+		{ name = 'nvim_lsp',               keyword_length = 3 }, -- from language server
+		{ name = 'nvim_lsp_signature_help' },                  -- display function signatures with current parameter emphasized
+		{ name = 'nvim_lua',               keyword_length = 2 }, -- complete neovim's Lua runtime API such vim.lsp.*
+		{ name = 'buffer',                 keyword_length = 2 }, -- source current buffer
+		{ name = 'calc' },                                     -- source for math calculation
+		{ name = 'luasnip',                keyword_length = 2 }, -- nvim-cmp source for LuaSnip
 	},
-
 	-- Use buffer source for `/` and `?` (if enabled `native_menu`, this won't work anymore)
 	cmp.setup.cmdline({ '/', '?' }, {
 		mapping = cmp.mapping.preset.cmdline(),
@@ -70,11 +66,10 @@ cmp.setup({
 			{ name = 'cmdline' }
 		})
 	}),
-
 	formatting = {
-		fields = {'menu', 'abbr', 'kind'},
+		fields = { 'menu', 'abbr', 'kind' },
 		format = function(entry, item)
-			local menu_icon ={
+			local menu_icon = {
 				nvim_lsp = 'λ',
 				luasnip = '⋗',
 				buffer = 'Ω',
@@ -85,3 +80,9 @@ cmp.setup({
 		end,
 	},
 })
+
+-- Autopairs options
+cmp.event:on(
+	'confirm_done',
+	require('nvim-autopairs.completion.cmp').on_confirm_done()
+)
