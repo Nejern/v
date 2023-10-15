2636654cd20f5b18415a0df75b35f5c0fcde7f02
Nejern <5qub6pfp@duck.com>
2023-10-15 12:26:49 +0300

    Updated keys

--- lua/keys/plugins.lua
+++ lua/keys/plugins.lua
@@ -3,12 +3,12 @@ require('keys.alias')
 g.mapleader = ' '
 
 -- LSP
-nm('K', '<cmd>lua vim.lsp.buf.hover()<CR>')        -- Hover for object
-nm('gf', '<cmd>lua vim.lsp.buf.format()<CR>')      -- Format document
-nm('ga', '<cmd>lua vim.lsp.buf.code_action()<CR>') -- Code actions
-nm('gR', '<cmd>lua vim.lsp.buf.rename()<CR>')      -- Rename object
+nm('K', '<cmd>lua vim.lsp.buf.hover()<CR>')          -- Hover for object
+nm('<leader>f', '<cmd>lua vim.lsp.buf.format()<CR>') -- Format document
+nm('ga', '<cmd>lua vim.lsp.buf.code_action()<CR>')   -- Code actions
+nm('gR', '<cmd>lua vim.lsp.buf.rename()<CR>')        -- Rename object
 
 -- FloaTerm configuration
-nm('<leader>ft', ':FloatermNew --name=myfloat --height=0.8 --width=0.7 --autoclose=2 zsh <CR>')
-nm('t', ':FloatermToggle myfloat<CR>')
+--nm('<leader>t', ':FloatermNew --name=myfloat --height=0.8 --width=0.7 --autoclose=2 zsh <CR>')
+nm('<leader>t', ':FloatermToggle myfloat<CR>')
 tm('<Esc>', '<C-\\><C-n>:q<CR>')
