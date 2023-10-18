6dc10b27455f35ef48afd09775cfc68c0f2b66f8
Nejern <5qub6pfp@duck.com>
2023-10-18 17:52:06 +0300

    Added indent plugins

--- lua/plugins/lazy.lua
+++ lua/plugins/lazy.lua
@@ -30,6 +30,7 @@ require("lazy").setup(
     },
     { -- Statusline
       "nvim-lualine/lualine.nvim",
+      event = "VeryLazy",
       dependencies = { "kyazdani42/nvim-web-devicons" },
       config = function()
         require("plugins.lualine")
@@ -47,6 +48,61 @@ require("lazy").setup(
       config = function()
         require("plugins.todo-comments")
       end
+    },
+    { -- Indent
+      "lukas-reineke/indent-blankline.nvim",
+      event = "VeryLazy",
+      opts = {
+        indent = {
+          char = "│",
+          tab_char = "│",
+        },
+        scope = { enabled = false },
+        exclude = {
+          filetypes = {
+            "help",
+            "alpha",
+            "dashboard",
+            "neo-tree",
+            "Trouble",
+            "lazy",
+            "mason",
+            "notify",
+            "toggleterm",
+            "lazyterm",
+          },
+        },
+      },
+      main = "ibl",
+    },
+    { -- Indent animation
+      "echasnovski/mini.indentscope",
+      version = false, -- wait till new 0.7.0 release to put it back on semver
+      event = "VeryLazy",
+      opts = {
+        -- symbol = "▏",
+        symbol = "│",
+        options = { try_as_border = true },
+      },
+      init = function()
+        vim.api.nvim_create_autocmd("FileType", {
+          pattern = {
+            "help",
+            "alpha",
+            "dashboard",
+            "neo-tree",
+            "Trouble",
+            "lazy",
+            "mason",
+            "notify",
+            "toggleterm",
+            "lazyterm",
+          },
+          callback = function()
+            vim.b.miniindentscope_disable = true
+          end,
+        })
+      end,
     },
 
     -- Completion --
