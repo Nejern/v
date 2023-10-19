3e049930e562c99eba121380f49c5fb7c0f17e14
Nejern <5qub6pfp@duck.com>
2023-10-19 15:37:55 +0300

    Suppress warning: undefined global 'vim'

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -27,7 +27,12 @@ require("mason-lspconfig").setup({
 require("mason-lspconfig").setup_handlers({
   function(server_name)
     require("lspconfig")[server_name].setup({
-      capabilities = require('cmp_nvim_lsp').default_capabilities()
+      capabilities = require('cmp_nvim_lsp').default_capabilities(),
+      settings = {
+        Lua = {
+          diagnostics = { globals = { 'vim' } },
+        },
+      },
     })
   end,
   -- Next, you can provide a dedicated handler for specific servers.
