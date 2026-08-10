d6abeab2ae18a00ca3a5f3862585cf80f73d9f8f
Oleg Nikitin <oenikitin@zlp.ooo>
2026-08-10 13:12:30 +0300

    feat: ssh clipboard

---
+++ lua/base/clipboard.lua
@@ -0,0 +1,8 @@
+opt.clipboard = "unnamedplus" -- System clipboard
+vim.g.clipboard = "osc52" -- OSC52 provider for SSH
+vim.api.nvim_create_autocmd("TextYankPost", { -- SSH
+	group = vim.api.nvim_create_augroup("highlight-yank", { clear = true }),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+})
